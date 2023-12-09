e0b7903ef21ac105c4a5ccede89d11912afa29df
Logic-H <hualogic@gmail.com>
2023-12-10 00:12:27 +0800

    delete old files

--- RandomMandala/__init__.py
+++
@@ -1,6 +0,0 @@
-"""RandomMandala package"""
-
-from RandomMandala.RandomMandala import RandomMandala
-from RandomMandala.RandomMandala import figure_to_image
-
-from RandomMandala.RandomFunctions import random_mandala
